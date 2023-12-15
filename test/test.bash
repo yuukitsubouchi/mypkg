0cfba6bdd881e6f2afcc317c0163c0d5f4d513a7
Tsubo Yuki <tytt3462@gmail.com>
2023-12-16 00:23:54 +0900

    Add a command

--- test/test.bash
+++ test/test.bash
@@ -1,9 +1,11 @@
 #!/bin/bash
+# SPDX-FileCopyrightText: 2023 Yuuki Tsubouchi 　　　　　
+# SPDX-License-Identifier: BSD-3-Clause
 
 dir=~
 [ "$1" != "" ] && dir="$1"   #引数があったら、そちらをホームに変える。
 
-cd $dir/ros2
+cd $dir/ros2_ws
 colcon build
 source $dir/.bashrc
 timeout 10 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
