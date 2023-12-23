31a27044b889a306d042d688cd56f2cfa74802e2
Tsubo Yuki <tytt3462@gmail.com>
2023-12-23 12:00:52 +0900

    test.bash

---
+++ test/suuretu_test.bash
@@ -0,0 +1,13 @@
+#!/bin/bash
+# SPDX-FileCopyrightText: 2023 Yuuki Tsubouchi 　　　　　
+# SPDX-License-Identifier: BSD-3-Clause
+
+dir=~
+[ "$1" != "" ] && dir="$1"   #引数があったら、そちらをホームに変える。
+
+cd $dir/ros2_ws
+colcon build
+source $dir/.bashrc
+timeout 15 ros2 launch mypkg suuretu_ans.launch.py > /tmp/mypkg.log
+cat /tmp/mypkg.log |
+grep '項：１５'
